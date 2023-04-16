2d87b7287b113ed58dc1c2974ec2f925fbea8d4f
miraamer <mira.amr13@gmail.com>
2023-04-16 20:28:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(n){
+    if (n >=42){
+        return n - 42;
+    }
+    else {
+        return 42 - n
+    }
+}
+
+function distanceFromHqInFeet(m){
+    const num = distanceFromHqInBlocks(m);
+    const blockLength = 264;
+    return num * blockLength;
+}
+
+function distanceTravelledInFeet(s,d){
+    const distance = Math.abs(s - d) * 264;
+    return distance;
+}
+
+function calculateFarePrice(s,d){
+    const distance = distanceTravelledInFeet(s,d);
+    if (distance <= 400){
+        return 0;
+    }
+    else if(distance >400 && distance <=2000){
+        let fee = (distance - 400) * 0.02;
+        return fee;
+    }
+    else if(distance > 2000 && distance <=2500){
+        return 25;
+    }
+    else if(distance >2500){
+        return "cannot travel that far";
+    }
+}
+
